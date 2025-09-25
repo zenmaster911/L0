d28a61101b2d070722ca3ebf25ac6a101785dae4
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-09-25 16:45:51 +0300

    test updated

--- pkg/handler/order_handler_test.go
+++ pkg/handler/order_handler_test.go
@@ -21,12 +21,12 @@ func TestHandler_CreateOrder(t *testing.T) {
 	type mockBehaviour func(s *mocks.OrderMock, ca *cache_mocks.RedisCacheInterfaceMock, reply *model.Reply)
 
 	testTable := []struct {
-		name                string
-		inputArgs           model.Reply
-		inputErr            error
-		mockBehavior        mockBehaviour
-		expectedStatusCode  int
-		expectedRequestBody string
+		name                 string
+		inputArgs            model.Reply
+		inputErr             error
+		mockBehavior         mockBehaviour
+		expectedStatusCode   int
+		expectedResponseBody string
 	}{
 		{
 			name: "OK",
@@ -83,8 +83,63 @@ func TestHandler_CreateOrder(t *testing.T) {
 			mockBehavior: func(s *mocks.OrderMock, ca *cache_mocks.RedisCacheInterfaceMock, reply *model.Reply) {
 				s.CreateOrderMock.Expect(reply).Return("b563feb7b2b84b6test", nil)
 			},
-			expectedStatusCode:  201,
-			expectedRequestBody: `{"order_uid":"b563feb7b2b84b6test"}`,
+			expectedStatusCode:   201,
+			expectedResponseBody: `{"order_uid":"b563feb7b2b84b6test"}`,
+		}, {
+			name: "validation fail",
+			inputArgs: model.Reply{
+				OrderUid:    "b563feb7b2b84b6test",
+				TrackNumber: "WBILMTESTTRACK",
+				Entry:       "WBIL",
+				Delivery: model.Delivery{
+					Name:    "test test",
+					Phone:   "",
+					Zip:     "111111",
+					City:    "test",
+					Address: "test test test",
+					Region:  "test",
+					Email:   "test@tes.ru",
+				},
+				Payment: model.Payment{
+					Transaction:  "b563feb7b2b84b6test",
+					RequestId:    "",
+					Currency:     "USD",
+					Provider:     "wbpay",
+					Amount:       12345,
+					PaymentDt:    1637907727,
+					Bank:         "test",
+					DeliveryCost: 100,
+					GoodsTotal:   111,
+					CustomFee:    0,
+				},
+				Items: []model.DeliveryItem{
+					model.DeliveryItem{
+						ChrtId:      11111,
+						TrackNumber: "WBILMTESTTRACK",
+						Price:       12345,
+						Rid:         "ab4219087a764ae0btest",
+						Name:        "tester",
+						Sale:        1,
+						Size:        "1",
+						TotalPrice:  12345,
+						NmId:        11111,
+						Brand:       "test",
+						Status:      202,
+					},
+				},
+				Locale:            "en",
+				InternalSignature: "",
+				CustomerId:        "test",
+				DeliveryService:   "test",
+				Shardkey:          "9",
+				SmId:              99,
+				DateCreated:       "2021-11-26T06:22:19Z",
+				OofShard:          "1",
+			},
+			inputErr:             nil,
+			mockBehavior:         func(s *mocks.OrderMock, ca *cache_mocks.RedisCacheInterfaceMock, reply *model.Reply) {},
+			expectedStatusCode:   http.StatusBadRequest,
+			expectedResponseBody: `{"error":"Validation failed","fields":["Phone is required"]}`,
 		},
 	}
 	for _, tt := range testTable {
@@ -109,8 +164,19 @@ func TestHandler_CreateOrder(t *testing.T) {
 
 			assert.Equal(t, tt.expectedStatusCode, w.Code)
 
-			assert.JSONEq(t, tt.expectedRequestBody, w.Body.String(), w.Body.String())
+			assert.JSONEq(t, tt.expectedResponseBody, w.Body.String(), w.Body.String())
 
 		})
+	}
+}
+
+func TestHandler_GetOrder(t *testing.T) {
+	type mockBehaviour func(s *mocks.OrderMock, ca *cache_mocks.RedisCacheInterfaceMock, reply *model.Reply)
+
+	testTable := []struct {
+		name         string
+		inputArgs    model.Reply
+		inputErr     error
+		mockBehavior mockBehaviour
 	}
 }
